404b85d0080bfe02617ead4d70ed67257b26bb98
Raul Gomez <rgomez@tec.mx>
2025-08-01 13:33:22 -0600

    Setting up data directory

--- src/Biopython_learning/main.py
+++ src/Biopython_learning/main.py
@@ -1,5 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Author: Raul Gomez
+Date: 2025-08-01
+Description: 
+"""
+from pathlib import Path
+
+# This assumes you're running the notebook or IPython session from the project root
+PROJECT_ROOT = Path().resolve()
+DATA_DIR = PROJECT_ROOT / "data"
+
+
 def main():
-    print("Hello from Biopython_learning!")
+    print(PROJECT_ROOT, "is the project root directory.")
+    print(DATA_DIR, "is the data directory.")
+
 
 if __name__ == "__main__":
     main()
