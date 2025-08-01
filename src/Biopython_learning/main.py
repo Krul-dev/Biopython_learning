41ee99bfb6eb427d6089616a82d872a1a3ec8d66
Raul Gomez <rgomez@tec.mx>
2025-08-01 14:25:10 -0600

    Setting up data directory

--- src/Biopython_learning/main.py
+++ src/Biopython_learning/main.py
@@ -6,15 +6,21 @@ Date: 2025-08-01
 Description: 
 """
 from pathlib import Path
+from Bio import SeqIO
 
 # This assumes you're running the notebook or IPython session from the project root
-PROJECT_ROOT = Path().resolve()
+PROJECT_ROOT = Path(__file__).resolve().parents[2]
 DATA_DIR = PROJECT_ROOT / "data"
 
 
 def main():
-    print(PROJECT_ROOT, "is the project root directory.")
-    print(DATA_DIR, "is the data directory.")
+    fasta_file_path = DATA_DIR / "ls_orchid.fasta"
+    sequences = SeqIO.parse(fasta_file_path, "fasta")
+    for record in sequences:
+        print(f"ID: {record.id}, Length: {len(record.seq)}")
+        print(f"Description: {record.description}")
+        # Print first 50 characters of the sequence
+        print(f"Sequence: {record.seq}")
 
 
 if __name__ == "__main__":
